68bfbbb52f19d61cb0da2a37486329507180c41b
seasons986 <annalin12345@gmail.com>
2025-05-08 15:03:34 -0400

    updated functions

--- functions.php
+++ functions.php
@@ -22,11 +22,11 @@ add_theme_support('title-tag');
  *
  * @return none
  */
-function GA_Theme_enqueue() {
+function Custom_Theme_enqueue() {
 //     wp_enqueue_style( 'rtl', get_template_directory_uri() . '/rtl.css' );
     wp_enqueue_style( 'normalize', get_template_directory_uri() . '/normalize.css' );
     wp_enqueue_style('fonts', get_stylesheet_directory_uri() . '/fonts.css', array(), '0.0.1', 'all');
     wp_enqueue_style('CustomThemeName', get_stylesheet_uri(), array(), '0.0.1', 'all');
     wp_enqueue_script('index', get_template_directory_uri() . '/scripts/index.js', array(), '0.0.1', true);
 }
-add_action('wp_enqueue_scripts', 'GA_Theme_enqueue');
+add_action('wp_enqueue_scripts', 'Custom_Theme_enqueue');
