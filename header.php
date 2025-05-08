a4f50a668842d29e523f21c8793bf7d3b66bbcd9
seasons986 <annalin12345@gmail.com>
2025-05-08 15:14:21 -0400

    update

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
       <meta charset="<?php bloginfo('charset'); ?>">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta property="og:type" content="website">
-      <meta property="og:url" content="https://2025.grayareafestival.io/">
+      <meta property="og:url" content="SITE_URL_HERE">
       <meta property="og:title" content="<?php bloginfo( 'name' ); ?>">
       <meta property="og:description" content="<?php bloginfo( 'description' ); ?>">
       <meta property="og:image" content="assets/favicon.png">
@@ -31,5 +31,4 @@
       </a>
     </h1>
     <p class="site-description"><?php bloginfo( 'description' ); ?></p>
-    <div class="header-link"><a href="https://tickets.grayarea.org/events/01905aac-8f29-7852-403f-d2432ec1aefe" class="ga-button-link">GET EARLY BIRD PASS</a></div>
   </header><!-- .site-header -->
--- push.sh
+++ push.sh
@@ -4,4 +4,3 @@ git cm "$1"
 git push
 echo "🍍make sure to change version numbers"
 rsync -avc --delete ./* USERNAME@SITENAME.ssh.wpengine.net:/sites/SITENAME/wp-content/themes/CustomThemeName/
-#rsync -avc --delete ./CustomThemeName/scripts/index.js gafest2025@gafest2025.ssh.wpengine.net:/sites/gafest2025/wp-content/themes/CustomThemeName/scripts/index.js
